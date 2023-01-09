d07502667d6711ed56d9f6ef1e96e567ab55ec19
Sanjay <54826834+sanjay75139@users.noreply.github.com>
2023-01-09 16:00:27 +0530

    Update projectsdata1.js

--- src/pages/projectsdata1.js
+++ src/pages/projectsdata1.js
@@ -4,7 +4,7 @@ const projectsdata1 = [
       image: "./freelance1.svg",
       description:
         "No compromise in the quality of products and the service you receive. Value for money is the name of the game",
-      link: "https://locuscorner.com/",
+      link: "Client's request is not to display",
     },
     {
       title: "Dharanifood",
@@ -12,7 +12,7 @@ const projectsdata1 = [
       description:
         "An online e-food shoping website.Order here and receive at your door",
          
-      link: "https://dharanifood.com/",
+      link: "Client's request is not to display",
     },
     {
       title: "ADNOX",
@@ -20,7 +20,7 @@ const projectsdata1 = [
       description:
         "Buy ADNOX Clothing & Footwear Online for men, women & kids in India. Explore a wide range of Max t-shirts, tops, jeans, shoes & more",
          
-      link: "https://adnoxfashion.com/",
+      link: "Client's request is not to display",
     },
     
   ];
