9d32d2875383e5a9d2f3c3c50891d7226fcd2fcf
Sanjay <54826834+sanjay75139@users.noreply.github.com>
2023-01-09 13:44:19 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -32,7 +32,7 @@ function Home() {
         </div>
       </div>
 
-      /*<Courosel />*/
+      {/* <Courosel /> */}
 
       <div>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
